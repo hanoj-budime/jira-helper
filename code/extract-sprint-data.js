b07dff294125bc31aec049e4bd9c03fb3d313e35
Hanoj Budime <Hanoj1b@gmail.com>
2024-08-07 10:59:16 +0530

    👩‍💻 Code changes

--- code/extract-sprint-data.js
+++ code/extract-sprint-data.js
@@ -3,33 +3,33 @@ let data = {};
 
 // * Iterate over each issue in the active sprint
 $(".ghx-sprint-active .ghx-row").each(function () {
-  // Extract the issue ID
+  // ? Extract the issue ID
   let id = $(this).find(".ghx-key").text();
-  // Extract the summary text of the issue
+  // ? Extract the summary text of the issue
   let text = $(this).find(".ghx-inner").text();
-  // Extract the issue type (e.g., Bug, Story)
+  // ? Extract the issue type (e.g., Bug, Story)
   let type = $(this).find(".ghx-type").attr("title");
-  // Extract the priority of the issue
+  // ? Extract the priority of the issue
   let priority = $(this).find(".ghx-priority").attr("title");
-  // Extract the story points of the issue
+  // ? Extract the story points of the issue
   let points = $(this).find(".ghx-statistic-badge").text();
-  // Clean up the ID by trimming any extra whitespace
+  // ? Clean up the ID by trimming any extra whitespace
   let code = id.trim();
 
-  // Store the extracted data in the 'data' object
+  // * Store the extracted data in the 'data' object
   data[code] = {
-    // Store the cleaned-up ID
-    // code: id.trim(),
-    // Store the cleaned-up summary text
+    // ? Store the cleaned-up ID
+    // ? code: id.trim(),
+    // ? Store the cleaned-up summary text
     summary: text.trim(),
-    // Store the cleaned-up issue type
+    // ? Store the cleaned-up issue type
     type: type.trim(),
-    // Store the cleaned-up priority
+    // ? Store the cleaned-up priority
     priority: priority.trim(),
-    // Store the cleaned-up and converted story points
+    // ? Store the cleaned-up and converted story points
     points: Number(points.trim()),
   };
 });
 
-// Output the data in a table format in the console
+// * Output the data in a table format in the console
 console.table(data);
